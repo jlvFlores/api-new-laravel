d8d38ec5d0327dde3ed215e31febca6980e6e51b
Luis Vazquez <jluisvazquez.jv@gmail.com>
2021-02-25 11:34:38 -0600

    5_8 Delete con TDD

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -70,6 +70,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+
+        return response()->json(null, 204);
+        
     }
 }
